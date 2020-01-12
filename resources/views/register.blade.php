f6b2da1a7ebb05a6126c9fd1527fb21aa79e1057
rexsteroxy <rexsteroxy22@gmail.com>
2020-01-12 20:18:41 +0100

    fixed a frontnd bug

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -50,6 +50,11 @@
               <div class="row align-items-center">
                 <div class="col-lg-6 mb-4">
                   <h1 data-aos="fade-up" data-aos-delay="100">Learn From The Expert</h1>
+                  @if (session('response'))
+                        <div class="alert alert-success">
+                            {{ session('response') }}
+                        </div>
+            @endif
                   <p class="mb-4" data-aos="fade-up" data-aos-delay="200">A 5 months practical oriented training class. 
             There will be a tutor lead section,<br> class programming session and a project development section.
              Every student will be expected to complete a web project (a Computer Based Test
@@ -66,11 +71,7 @@
                 </div>
                
                 <div class="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay="500">
-                @if (session('response'))
-                        <div class="alert alert-success">
-                            {{ session('response') }}
-                        </div>
-            @endif
+                
                   <form action="/student-registration" method="post" class="form-box">
                     {{ csrf_field() }}
 
@@ -111,150 +112,7 @@
     </div>
 
 
-   <br><br><br><br><br>
-
-    <div class="site-section bg-light" id="contact-section">
-      <div class="container">
-
-        <div class="row justify-content-center">
-          <div class="col-md-7">
-
-
-
-            <h2 class="section-title mb-3">Message Us</h2>
-            <p class="mb-5">Tell us about your tech related problems so we can know how best to serve you. 
-            Kindly fill the contact form.</p>
-             
-
-            <form action="/message" method="post">
-            {{ csrf_field() }}
-              <div class="form-group row">
-                <div class="col-md-6 mb-3 mb-lg-0">
-                  <input type="text" class="form-control" placeholder="First name" name="firstname">
-                </div>
-                <div class="col-md-6">
-                  <input type="text" class="form-control" placeholder="Last name" name="lastname">
-                </div>
-              </div>
- 
-              <div class="form-group row">
-                <div class="col-md-12">
-                  <input type="email" class="form-control" placeholder="Email" name="email">
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <div class="col-md-12">
-                  <input type="text" class="form-control" placeholder="Subject" name="subject">
-                </div>
-              </div>
-
-              
-              <div class="form-group row">
-                <div class="col-md-12">
-                  <textarea class="form-control" id="" cols="30" rows="10"
-                    placeholder="Write your message here." name="message"></textarea>
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <div class="col-md-6">
-
-                  <input type="submit" class="btn btn-primary py-3 px-5 btn-block btn-pill" value="Send Message">
-                </div>
-              </div>
-
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-
-
-    <!-- Google Map -->
-
-
-    
-      <div class="container">
-
-        <div class="row justify-content-center">
-          <div class="col-md-7">
-
-
-
-            <h2 class="section-title mb-3">See Our Location</h2>
-            <p class="mb-5">Easy To Locate</p>
-             
-            <iframe
-             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3971.536806589953!2d7.037129!3d5.4869767!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1042593f19f8a56d%3A0x3d116de9a709abfd!2sOwerri%20Tech%20Hub!5e0!3m2!1sen!2sng!4v1578828899318!5m2!1sen!2sng" width="auto" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-            </div>
-          </div>
-            
-          </div>
-          </div>
-        
-     
-    <!-- Ends here -->
-
-
-    <footer class="footer-section bg-white">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <h3>About OwerriTechHub</h3>
-            <p>We run IT training bootcamps with focus on best tools and practises used in the Tech ecosystem.
-             We also incubate startups with the right tools for business growth.</p>
-          </div>
-
-          <div class="col-md-3 ml-auto">
-            <h3>Links</h3>
-            <ul class="list-unstyled footer-links">
-            <li><a href="#home-section" >Home</a></li>
-                <li><a href="#courses-section" >Co-Working Space</a></li>
-                <li><a href="#programs-section">Programs</a></li>
-                <li><a href="#teachers-section" >About Us</a></li>
-            </ul>
-          </div>
-
-          <div class="col-md-4">
-          <h3>Contact Us</h3>
-          <ul class="list-unstyled footer-links">
-            <li> <span class="icon icon-phone"></span> +2348171904344</li>
-            <li><span class="icon icon-phone"></span> +2348067915982</li>
-            <li><span class="icon-chat"></span> info@owerritechhub.com</li>
-            </ul>
-            
-            <p>Subscribe to recieve the hub's updates.</p>
-            <form action="#" class="footer-subscribe">
-              <div class="d-flex mb-5">
-                <input type="text" class="form-control rounded-0" placeholder="Email">
-                <input type="submit" class="btn btn-primary rounded-0" value="Subscribe">
-              </div>
-            </form>
-          </div>
-
-        </div>
-
-        <div class="row pt-5 mt-5 text-center">
-          <div class="col-md-12">
-            <div class="border-top pt-5">
-              <p>
-                
-                Copyright &copy;
-                <script>document.write(new Date().getFullYear());</script> All rights reserved | Developed
-                with <i class="icon-heart" aria-hidden="true"></i> by <a href=""
-                  target="_blank">Rexsteroxy</a>
-               
-              </p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </footer>
-
+   
 
 
   </div> <!-- .site-wrap -->
